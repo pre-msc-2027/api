83429059235f67eaf2047b92f921a479bbdebfc7
Jaures <jauresacs2004@gmail.com>
2025-06-24 14:07:57 +0200

    feat: configuration to get all informations to user on github

--- src/core/app.py
+++ src/core/app.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
-from src.routes import rules 
-
+from src.routes import rules
+from src.user import router as user_router
+from src.routes.repo import router as repo_router
 app = FastAPI()
 
 app.include_router(rules.router)
+
+app.include_router(user_router, prefix="/user")
+app.include_router(repo_router, prefix="/repo")
