05c858dc70f314bd8c05849fac3eedb5483b184e
Parisel Guillaume <guillaume.parisel@epitech.eu>
2025-07-15 15:13:08 +0200

    update

--- src/models/repo.py
+++ src/models/repo.py
@@ -1,10 +1,28 @@
-from typing import List
+from typing import List, Any
 from beanie import Document
+from pydantic import BaseModel
+
+
+class RuleParameter(BaseModel):
+    name: str
+    value: Any
+
+
+class RepoRule(BaseModel):
+    rule_id: str
+    parameters: List[RuleParameter] 
+
+
+class RepoUser(BaseModel):
+    id: str 
+    email: str
+    name: str | None
+
 
 class Repo(Document):
+    user: RepoUser
     repo_url: str
-    user: str
-    rules_list: List[int]
+    rules: List[RepoRule]
 
     class Settings:
         name = "repos"
--- src/schemas/rule.py
+++ src/schemas/rule.py
@@ -1,12 +1,26 @@
 from pydantic import BaseModel
+from typing import List, Any, Dict, Optional
+
+
+class RuleParameterSchema(BaseModel):
+    type: str
+    name: str
+    default: Any
+    description: Optional[str] = None
+    options: Optional[Dict[str, Any]] = None
+
 
 class RuleBase(BaseModel):
-    rule_id: int
+    rule_id: str
     name: str
-    description: str | None = None
+    description: Optional[str] = None
+    tags: List[str] = []
+    parameters: List[RuleParameterSchema] = []
+
 
 class RuleCreate(RuleBase):
     pass
 
+
 class RuleOut(RuleBase):
-    pass 
+    pass
