57edfee5d14c32a0fa24057a7fbf6fe56ae55c83
Parisel Guillaume <guillaume.parisel@epitech.eu>
2025-09-02 11:50:46 +0200

    repo dans ia

--- src/schemas/ai.py
+++ src/schemas/ai.py
@@ -4,5 +4,6 @@ from src.schemas.scan import AnalysisSchema
 from src.schemas.rule import RuleOut
 
 class AnalysisWithRulesResponse(BaseModel):
+    repo_url: str
     analysis: AnalysisSchema
     rules: List[RuleOut]
